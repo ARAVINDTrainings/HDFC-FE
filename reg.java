bbf247b6467202bb7396895bf8d135f8dd56faf7
Aravind <Aravind@gmail.com>
2026-05-12 15:22:59 +0000

    reg welcome

---
+++ reg.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        // This prints text to the console
+        System.out.println("Hello, World!");
+    }
+}
+
---
+++ welcome.java
@@ -0,0 +1,13 @@
+public class SimpleMath {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        
+        int sum = a + b;
+        int difference = a - b;
+        
+        System.out.println("Sum: " + sum);
+        System.out.println("Difference: " + difference);
+    }
+}
+
